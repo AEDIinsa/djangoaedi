94b3fc4958e41c39451d494bb0475e5ea540643e
Hugo David-Boyet <hugo.david.boyet@gmail.com>
2017-01-08 13:37:05 +0100

    Add TaigaAccount model

--- aedisite/models.py
+++ aedisite/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class TaigaAccount(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.BooleanField()
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        permissions = (
+                ("create_taiga", "Can create a new Taiga account."),
+        )
+
+
